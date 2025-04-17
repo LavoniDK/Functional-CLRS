2cd005bb8ba18d6c1914caf17ada2ed44350fc5e
Laust Volsgaard Nielsen <145433012+LavoniDK@users.noreply.github.com>
2025-04-17 15:42:22 +0200

    Add files via upload

--- binary-heaps/BinaryHeaps.hs
+++ binary-heaps/BinaryHeaps.hs
@@ -1,42 +1,97 @@
-module BinaryHeaps (parent, left, right, maxHeapify) where
-    
-parent :: Int -> Int
-parent i = div i 2
+module BinaryHeaps (
+    Tree(..), 
+    buildMaxHeap,
+    heapSort,
+    maxHeapInsert,
+    maxHeapExtractMax,
+    maxHeapIncreaseKey, 
+    heapSize,
+    listToTree,
+    treeToList
+) where
 
-left :: Int -> Int
-left i = 2 * i  
+data Tree a = Nil | Node a (Tree a) (Tree a) 
+    deriving (Show)
 
-right :: Int -> Int
-right i = 2 * i + 1
+maxHeapify :: (Ord a) => Tree a -> Tree a
+maxHeapify Nil = Nil
+maxHeapify t@(Node c Nil Nil) = t
+maxHeapify t@(Node c (Node lc ll lr) Nil)
+    | lc > c    = Node lc (maxHeapify (Node c ll lr)) Nil
+    | otherwise = t
+maxHeapify t@(Node c l@(Node lc ll lr) r@(Node rc rl rr))
+    | lc > c, lc >= rc = Node lc (maxHeapify (Node c ll lr)) r
+    | rc > c, rc >= lc = Node rc l (maxHeapify (Node c rl rr))
+    | otherwise = t
 
-swap :: [a] -> Int -> Int -> [a]
-swap xs i j = let a = xs !! i
-                  b = xs !! j
-              in take i xs ++ [b] ++ drop (i + 1) (take j xs) ++ [a] ++ drop (j + 1) xs
+buildMaxHeap :: (Ord a) => Tree a -> Tree a
+buildMaxHeap t@(Node c Nil _) = t
+buildMaxHeap (Node c l r) = maxHeapify (Node c (buildMaxHeap l) (buildMaxHeap r))
 
-maxHeapify :: [Int] -> Int -> [Int]
-maxHeapify xs i
-    | largest /= i = maxHeapify (swap xs i largest) largest
-    | otherwise = xs
+heapSort :: Ord a => [a] -> [a]
+heapSort [] = []
+heapSort xs = extractAll $ buildMaxHeap $ listToTree xs
+  where
+    extractAll Nil = []
+    extractAll t   = let (maxVal, newHeap) = maxHeapExtractMax t
+                     in maxVal : extractAll newHeap
+
+-- Due to list-tree transformation this function is O(n), not O(log n) - i am missing the smart recursive solution
+maxHeapExtractMax :: (Ord a) => Tree a -> (a, Tree a)
+maxHeapExtractMax (Node c Nil _) = (c, Nil)
+maxHeapExtractMax (Node c (Node lc _ _) Nil) = (c, Node lc Nil Nil)
+maxHeapExtractMax t@(Node c l r) = (c, maxHeapify $ listToTree $ swapNdrop $ treeToList t)
+  where
+    swapNdrop xs = last xs : tail (init xs)
+
+maxHeapIncreaseKey :: (Ord a) => Tree a -> Int -> a -> Tree a  
+maxHeapIncreaseKey (Node c l r) 1 k
+  | k < c     = error "new key is smaller than current key"
+  | otherwise = Node k l r
+maxHeapIncreaseKey (Node c l r) i k
+  | even i = bubbleFromLeft (Node c (maxHeapIncreaseKey l (div i 2) k) r)
+  | otherwise = bubbleFromRight (Node c l (maxHeapIncreaseKey r (div i 2) k))
+    where 
+      bubbleFromLeft (Node p l@(Node lc ll lr) r)
+        | lc > p    = Node lc (Node p ll lr) r
+        | otherwise = Node p l r
+      bubbleFromRight (Node p l r@(Node rc rl rr))
+        | rc > p    = Node rc l (Node p rl rr)
+        | otherwise = Node p l r
+
+maxHeapInsert :: (Ord a, Bounded a) => Tree a -> a -> Tree a
+maxHeapInsert Nil k = Node k Nil Nil 
+maxHeapInsert t@(Node c l r) k
+  | heapSize l <= heapSize r = maxHeapIncreaseKey (Node c (maxHeapInsert' l) r) (heapSize t + 1) k
+  | otherwise                = maxHeapIncreaseKey (Node c l (maxHeapInsert' r)) (heapSize t + 1) k
+  where 
+    size = heapSize t + 1
+
+maxHeapInsert' :: (Ord a, Bounded a) => Tree a -> Tree a  
+maxHeapInsert' (Node c Nil Nil) = Node c (Node minBound Nil Nil) Nil 
+maxHeapInsert' (Node c l Nil) = Node c l (Node minBound Nil Nil)
+maxHeapInsert' (Node c l r)
+  | heapSize l <= heapSize r = (Node c (maxHeapInsert' l) r) 
+  | otherwise                = (Node c l (maxHeapInsert' r))
+
+-- Auxilliary functions
+heapSize :: Tree a -> Int
+heapSize Nil = 0
+heapSize (Node c l r) = 1 + heapSize l + heapSize r
+
+treeToList :: Tree a -> [a]
+treeToList t = bfs [t]
+  where
+    bfs [] = []
+    bfs (Nil : xs) = bfs xs
+    bfs (Node c l r : xs) = c : bfs (xs ++ [l, r])
+
+listToTree :: (Ord a) => [a] -> Tree a
+listToTree xs = build 0
   where
     n = length xs
-    l = left i
-    r = right i
-    largest
-        | l < n && xs !! l > xs !! i = 
-            if r < n && xs !! r > xs !! l then r else l
-        | r < n && xs !! r > xs !! i = r
-        | otherwise = i
-
-buildMaxHeap :: [Int] -> [Int]
-buildMaxHeap [] = []
-buildMaxHeap xs = 
-    buildMaxHeap init heap ++ last heap
-        where mid = div (length xs) 2
-              heap = maxHeapify xs mid
-
-
-main :: IO ()
-main = do
-    let heap = [3, 2, 1, 7, 8, 4, 10, 16, 12]
-    print $ maxHeapify heap 0
+    build i
+      | i >= n      = Nil
+      | 2*i + 1 > n = Node (xs !! i) Nil Nil
+      | 2*i + 2 > n = Node (xs !! i) (build (2*i + 1)) Nil
+      | otherwise   = Node (xs !! i) (build (2*i + 1)) (build (2*i + 2))
---
+++ binary-heaps/TestBinaryHeaps.hs
@@ -0,0 +1,56 @@
+module TestBinaryHeaps where
+
+import BinaryHeaps (
+    Tree(..), 
+    buildMaxHeap,
+    heapSort,
+    maxHeapInsert,
+    maxHeapExtractMax,
+    maxHeapIncreaseKey,
+    heapSize,
+    listToTree,
+    treeToList) 
+
+maxHeapMax :: Tree a -> a
+maxHeapMax Nil = error "Empty heap has no maximum"
+maxHeapMax (Node c _ _) = c
+
+testAssert :: (Eq a, Show a) => String -> a -> a -> IO ()
+testAssert testName result expected =
+    if result == expected
+        then putStrLn $ "[PASS] " ++ testName
+        else putStrLn $ "[FAIL] " ++ testName ++
+                        "\n  Expected: " ++ show expected ++
+                        "\n  Got:      " ++ show result
+
+main :: IO ()
+main = do
+    putStrLn "Running BinaryHeaps tests..."
+
+    -- buildMaxHeap (visually checked)
+    let tree = listToTree [3, 5, 1, 10, 2]
+    let built = buildMaxHeap tree
+    testAssert "buildMaxHeap: max element at root" (maxHeapMax built) 10
+
+    -- heapSort
+    testAssert "heapSort: sort ascending" (heapSort [3, 5, 1, 10, 2]) [10, 5, 3, 2, 1]
+    testAssert "heapSort: already sorted" (heapSort [9, 8, 7]) [9, 8, 7]
+    testAssert "heapSort: reversed input" (heapSort [1, 2, 3]) [3, 2, 1]
+
+    -- maxHeapInsert
+    let tree1 = buildMaxHeap (listToTree ([4, 2, 3] :: [Int]))
+    let inserted = maxHeapInsert tree1 (10 :: Int)
+    testAssert "maxHeapInsert: 10 becomes new root" (maxHeapMax inserted) 10
+
+    -- maxHeapExtractMax
+    let tree2 = buildMaxHeap (listToTree [8, 3, 6, 1, 4])
+    let (max1, after1) = maxHeapExtractMax tree2
+    testAssert "maxHeapExtractMax: returns max" max1 8
+    testAssert "maxHeapExtractMax: second max is next root" (maxHeapMax after1) 6
+
+    -- maxHeapIncreaseKey
+    let tree3 = buildMaxHeap (listToTree [4, 2, 3])
+    let increased = maxHeapIncreaseKey tree3 3 9  -- assume position 3 is leaf with value 3
+    testAssert "maxHeapIncreaseKey: bubbling up to root" (maxHeapMax increased) 9
+
+    putStrLn "All tests completed."
